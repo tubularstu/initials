2da673070741713fc47366e3674a0fdb869822ca
tubularstu <75602604+tubularstu@users.noreply.github.com>
2022-01-13 15:37:16 -0600

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,20 @@
+#Name: Stuart Alexander
+#Fun Fact: ASCII art is actually dope
+
+initials = """
+ SSS     A
+S   S   A A
+S      A   A
+ SSS   AAAAA
+    S  A   A
+S   S  A   A
+ SSS   A   A"""
+print (initials)
+
+print (' SSS     A  ')
+print ('S   S   A A')
+print ('S      A   A')
+print (' SSS   AAAAA')
+print ('    S  A   A')
+print ('S   S  A   A')
+print (' SSS   A   A')
